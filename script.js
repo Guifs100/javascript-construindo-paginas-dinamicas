340a3b33e1f3c8b4d3902ff4114098ac4cefdf69
Guilherme Feitosa Sampaio <guifs100@gmail.com>
2025-07-27 11:46:05 -0300

    fim do cap 2 e 3

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 const inputItem = document.getElementById('input-item');
+const listaCompras = document.getElementById('lista-de-compras');
 // let text = document.querySelector('#titulo-lista');
 // text.innerHTML = 'Lista de compras atualizada';
 
 // console.log(text);
 
 const botaoAdicionar = document.getElementById('adicionar-item');
+let count = 0;
+
 botaoAdicionar.addEventListener('click', (event) => {
   // console.log(event.target);
   event.preventDefault(); // Previne o comportamento padrão do botão
   console.log(inputItem.value);
+  if (inputItem.value === '') {
+    alert('Por favor, insira um item.');
+    return;
+  }
+
+  const itemList = document.createElement('li');
+  const containerItemList = document.createElement('div');
+  containerItemList.classList.add('lista-item-container');
+  const inputCheckbox = document.createElement('input');
+  inputCheckbox.type = 'checkbox';
+  inputCheckbox.id = `item-${count++}`;
+  const nameItem = document.createElement('p');
+  nameItem.innerText = inputItem.value;
+
+  inputCheckbox.addEventListener('click', function() {
+    if (inputCheckbox.checked) {
+      nameItem.style.textDecoration = 'line-through';
+      nameItem.style.color = 'gray';
+    } else {
+      nameItem.style.textDecoration = 'none';
+      nameItem.style.color = 'black';
+    }
+  }
+
+  containerItemList.appendChild(inputCheckbox);
+  containerItemList.appendChild(nameItem);
+  itemList.appendChild(containerItemList);
+  document.getElementById('lista').appendChild(itemList);
+  //listaCompras.appendChild(itemList);
+
+  const diaDaSemana = new Date().toLocaleDateString('pt-BR', {weekday: 'long'});
+  const data = new Date().toLocaleDateString('pt-BR');
+  const hora = new Date().toLocaleTimeString('pt-BR', {hour: 'numeric', minute: 'numeric'});
+
+  const dataCompleta = `${diaDaSemana} (${data}) ás ${hora}`;
+  const itemData = document.createElement('p');
+  itemData.classList.add('data-item');
+  itemData.innerHTML = dataCompleta;
+
+  itemList.appendChild(itemData);
+
+  listaCompras.appendChild(itemList);
 });
